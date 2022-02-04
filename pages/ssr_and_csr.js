978e0518cbe1ca2d5743aaca25ddc2d8a2687513
Naoki Kaneshiro <kaneshiro@rightcode.co.jp>
2022-02-04 17:59:56 +0900

    SSRでもCSRでも動くサンプル

---
+++ pages/ssr_and_csr.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+function Page({ stars }) {
+  return <div>Next stars: {stars}</div>
+}
+Page.getInitialProps = async (ctx) => {
+  const res = await fetch('https://api.github.com/repos/vercel/next.js')
+  const json = await res.json()
+  return { stars: json.stargazers_count }
+}
+
+export default Page
